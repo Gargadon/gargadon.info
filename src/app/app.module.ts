19eac444c88a2ee6e955c8178fc6dd423e8c21ac
David Kantun <dkantun@gmail.com>
2024-06-28 03:31:41 -0600

    Updating weas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,10 +22,11 @@ import { MainComponent } from './proyectos/main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { DiceComponent } from './proyectos/dice/dice.component';
 import { Login2Component } from './proyectos/login/login.component';
-import { HttpClient,  provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi, withFetch } from '@angular/common/http';
 import { LoginComponent } from './login/login.component';
 import { SsrCookieService } from 'ngx-cookie-service-ssr';
 import { LogoutComponent } from './logout/logout.component';
+import { MarkdownModule } from 'ngx-markdown';
 
 
 var titulo = "Gargadon's Dungeon :: "
@@ -88,7 +89,8 @@ const appRoutes: Routes = [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    MarkdownModule.forRoot()
   ],
   providers: [provideHttpClient(withInterceptorsFromDi()), provideHttpClient(withFetch()), SsrCookieService, ServicioEmpleadosService, DataServices, EmpleadosService, provideClientHydration(withNoHttpTransferCache())],
   bootstrap: [AppComponent]
